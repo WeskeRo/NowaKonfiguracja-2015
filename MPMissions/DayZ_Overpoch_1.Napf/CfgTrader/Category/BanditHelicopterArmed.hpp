a0859088d99dca11d346e2962346c5559f327140
WeskeRo <wesker01@gmail.com>
2015-12-23 11:45:53 +0100

    uploading

---
+++ MPMissions/DayZ_Overpoch_1.Napf/CfgTrader/Category/BanditHelicopterArmed.hpp
@@ -0,0 +1,53 @@
+class Category_512 {
+		class CH_47F_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={400000,"Coins"};
+		sell[] ={200000,"Coins"};
+	};
+	class UH1H_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={400000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class Mi17_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={300000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class Mi17_CDF {
+		type = "trade_any_vehicle";
+		buy[] ={300000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class Mi17_Ins {
+		type = "trade_any_vehicle";
+		buy[] ={300000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class UH60M_EP1_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={500000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class UH1Y_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={400000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class MH60S_DZE {
+		type = "trade_any_vehicle";
+		buy[] ={400000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class UH1H_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={400000,"Coins"};
+		sell[] ={100000,"Coins"};	
+	};
+	class Mi17_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={500000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	
+};		
