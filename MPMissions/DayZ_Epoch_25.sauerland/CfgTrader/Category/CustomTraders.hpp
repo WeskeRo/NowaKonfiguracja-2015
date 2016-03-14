36c5cb264dc93d928034ec1791dcb41ac3064708
ilia <ilffla@yahoo.com>
2016-03-14 19:36:47 -0400

    Devaluation of Currency

--- MPMissions/DayZ_Epoch_25.sauerland/CfgTrader/Category/CustomTraders.hpp
+++ MPMissions/DayZ_Epoch_25.sauerland/CfgTrader/Category/CustomTraders.hpp
@@ -1,13 +1,13 @@
 class Category_693 {
-	class ItemKiloHemp {type = "trade_items";buy[] = {5000,"Coins"};sell[] = {2000,"Coins"};};
-	class ItemMorphine {type = "trade_items";buy[] = {1000,"Coins"};sell[] = {500,"Coins"};};
-	class ItemPainkiller {type = "trade_items";buy[] = {2000,"Coins"};sell[] = {500,"Coins"};};
+	class ItemKiloHemp {type = "trade_items";buy[] = {500,"Coins"};sell[] = {200,"Coins"};};
+	class ItemMorphine {type = "trade_items";buy[] = {100,"Coins"};sell[] = {50,"Coins"};};
+	class ItemPainkiller {type = "trade_items";buy[] = {200,"Coins"};sell[] = {50,"Coins"};};
 };
 
 class Category_695 {
-	class ItemObsidian {type = "trade_items";buy[] = {100000,"Coins"};sell[] = {100000,"Coins"};};
-	class ItemSapphire {type = "trade_items";buy[] = {100000,"Coins"};sell[] = {100000,"Coins"};};	
-	class ItemTopaz {type = "trade_items";buy[] = {100000,"Coins"};sell[] = {100000,"Coins"};};
-	class ItemRuby {type = "trade_items";buy[] = {100000,"Coins"};sell[] = {100000,"Coins"};};
-	class ItemEmerald {type = "trade_items";buy[] = {100000,"Coins"};sell[] = {100000,"Coins"};};
+	class ItemObsidian {type = "trade_items";buy[] = {10000,"Coins"};sell[] = {10000,"Coins"};};
+	class ItemSapphire {type = "trade_items";buy[] = {10000,"Coins"};sell[] = {10000,"Coins"};};	
+	class ItemTopaz {type = "trade_items";buy[] = {10000,"Coins"};sell[] = {10000,"Coins"};};
+	class ItemRuby {type = "trade_items";buy[] = {10000,"Coins"};sell[] = {10000,"Coins"};};
+	class ItemEmerald {type = "trade_items";buy[] = {10000,"Coins"};sell[] = {10000,"Coins"};};
 };
